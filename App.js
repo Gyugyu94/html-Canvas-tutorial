353cf1a36822fa609fe3ae9453ca9bccdcfb46f8
심규민 <simgyumin@simgyumin-ui-MacBookAir.local>
2021-01-01 17:43:31 +0900

    test

--- App.js
+++ App.js
@@ -4,7 +4,6 @@ class App {
     constructor() {
         this.canvas = document.createElement('canvas');
         this.ctx = this.canvas.getContext('2d');
-
         document.body.appendChild(this.canvas);
 
         window.addEventListener('resize', this.resize.bind(this), false);
